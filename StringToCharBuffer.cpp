5a8b31489e5590eff81cc9090509895c42d12f60
Roman Kafitulin <kafitulin1992@gmail.com>
2026-02-11 17:47:04 -0800

    implement StringToCharBuffer.cpp function

--- StringToCharBuffer.cpp
+++ StringToCharBuffer.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 
 
+void StringToCharBuffer(const string &inputString, char** outPtr )
+{
+    int length = inputString.length();
+
+    *outPtr = new char[length + 1];
+
+    char *writePtr = *outPtr;
+
+    for (int i = 0; i < length; i++) {
+        *writePtr = inputString[length - 1 - i]; 
+        writePtr++;
+    }
+
+    *writePtr = '\0';
+}
+
+
+
 int main()
 {
     char* p;
